d3ab6175906f07034aa77d16b4c65e8cb2e4f593
nazmulch11 <nazmul.ch11@gmail.com>
2018-07-02 09:12:46 +0600

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,10 +1,22 @@
 
+        public function __construct()
+        {
+            parent::__construct();
+            if (!$this->session->userdata('admin_session')) {
+                redirect('adminportal');
+            }
+
+            date_default_timezone_set('Asia/Dhaka');
+
+            $this->load->model('model_name');
+        }
+
 //this function recieved ajax request
 
 public function vendor_ajax_brand()
         {
             $vendor_id = $this->input->post('vendor_id');
-            $brand = $this->Vendor_model->get_brand_by_vendor($vendor_id);
+            $brand = $this->model_name->get_brand_by_vendor($vendor_id);
 
             echo json_encode($brand);
 
